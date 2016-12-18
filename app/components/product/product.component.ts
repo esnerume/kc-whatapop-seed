2473365306b3677f62058569e68bd399e0af577e
Rubén Merino Losada <rmerino@MacBook-Air-de-Ruben.local>
2016-12-18 10:17:28 +0100

    Green Path

--- app/components/product/product.component.ts
+++ app/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import {Component, Input, Output, EventEmitter} from "@angular/core";
 
 import { Product } from "../../models/product";
 
@@ -20,4 +20,9 @@ export class ProductComponent {
     | de este componente, necesitas, además, un manejador para el      |
     | mismo.                                                           |
     |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    @Output() verVistaDetalle:  EventEmitter<Product> = new EventEmitter();
+
+    notificarVerVistaDetalle(producto: Product): void {
+        this.verVistaDetalle.emit(producto);
+    }
 }
